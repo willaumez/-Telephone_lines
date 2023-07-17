cb8fc392a1e0aa76d46e68c20fefeeef64a420b7
willaumez <owanisanajency@gmail.com>
2023-07-17 10:52:45 +0100

    Fist commit

---
+++ BackendTelephoneLines/src/main/java/com/telephone/backendtelephonelines/BackendTelephoneLinesApplication.java
@@ -0,0 +1,13 @@
+package com.telephone.backendtelephonelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTelephoneLinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendTelephoneLinesApplication.class, args);
+    }
+
+}
---
+++ BackendTelephoneLines/src/test/java/com/telephone/backendtelephonelines/BackendTelephoneLinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telephone.backendtelephonelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTelephoneLinesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
