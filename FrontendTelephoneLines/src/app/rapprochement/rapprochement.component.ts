cae2fba003fb77bf2f2aa57174d95669094553f7
willaumez <owanisanajency@gmail.com>
2023-08-15 11:33:42 +0100

    15 08 23

--- FrontendTelephoneLines/src/app/rapprochement/rapprochement.component.ts
+++ FrontendTelephoneLines/src/app/rapprochement/rapprochement.component.ts
@@ -14,6 +14,14 @@ import {LigneTelephoniqueService} from "../services/ligne-telephonique.service";
 })
 export class RapprochementComponent implements OnInit {
   dataFromExcel: Rapprochement[] = [];
+  dataFromDataBase: Rapprochement[] = [];
+
+  dataCorresponding: Rapprochement[] = [];
+  dataMontantNoCor: Rapprochement[] = [];
+  dataNoExistDB: Rapprochement[] = [];
+  dataNoExistExcel: Rapprochement[] = [];
+
+
   isLoading: boolean = false;
   isFileDragging: boolean = false;
   selectedFile!: File | null;
@@ -22,6 +30,10 @@ export class RapprochementComponent implements OnInit {
   displayedColumns: string[] = ['numero', 'montant'];
   dataSource!: MatTableDataSource<any>;
   dataBase!: MatTableDataSource<any>;
+  tableCorresponding!: MatTableDataSource<any>;
+  tableMontantNoCor!: MatTableDataSource<any>;
+  tableNoExistDB!: MatTableDataSource<any>;
+  tableNoExistExcel!: MatTableDataSource<any>;
   //@ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
@@ -75,6 +87,7 @@ export class RapprochementComponent implements OnInit {
   getListLignes() {
     this.ligneService.getLignesRapprochement().subscribe({
       next: (data) => {
+        this.dataFromDataBase = data;
         this.dataBase = new MatTableDataSource(data);
         //this.dataSource.sort = this.sort;
         //this.dataSource.paginator = this.paginator;
@@ -96,8 +109,6 @@ export class RapprochementComponent implements OnInit {
 
           // Trigger manual change detection
           this.cdRef.detectChanges();
-
-          console.log('Contenu de dataFromExcel :', data);
         },
         (error) => {
           console.error('Erreur lors de l\'importation du fichier :', error);
@@ -121,5 +132,89 @@ export class RapprochementComponent implements OnInit {
     this.isFileDragging= false;
     this.selectedFile= null;
   }
+
+  /*traitementBtn() {
+    //comparaison des numeros
+    for (const excelItem of this.dataFromExcel) {
+      for (const dbItem of this.dataFromDataBase) {
+        if (excelItem.numero === dbItem.numero) {
+          this.dataCorresponding.push(excelItem);
+          break; // Passer au prochain élément de dataFromExcel dès qu'une correspondance est trouvée
+        }
+      }
+    }
+    this.tableCorresponding = new MatTableDataSource(this.dataCorresponding);
+    console.log('this.dataCorresponding :', this.dataCorresponding);
+    //comparaison des numeros montant
+    for (const correspondingItem of this.dataCorresponding) {
+      const excelItem = this.dataFromExcel.find(excelItem => excelItem.numero === correspondingItem.numero);
+
+      if (excelItem && excelItem.montant !== correspondingItem.montant) {
+        this.dataMontantNoCor.push(correspondingItem);
+      }
+    }
+    this.tableMontantNoCor = new MatTableDataSource(this.dataMontantNoCor);
+    console.log('this.dataMontantNoCor :', this.dataMontantNoCor);
+
+  }*/
+
+  traitementBtn() {
+    // Initialiser les listes pour les données inexistantes
+    //this.dataNoExistDB = [];
+    //this.dataNoExistExcel = [];
+
+    // Comparaison des numeros
+    for (const excelItem of this.dataFromExcel) {
+      let numeroExisteDansDB = false;
+
+      for (const dbItem of this.dataFromDataBase) {
+        if (excelItem.numero === dbItem.numero) {
+          this.dataCorresponding.push(excelItem);
+          numeroExisteDansDB = true;
+          break; // Passer au prochain élément de dataFromExcel dès qu'une correspondance est trouvée
+        }
+      }
+
+      if (!numeroExisteDansDB) {
+        this.dataNoExistDB.push(excelItem);
+      }
+    }
+
+    // Comparaison des numeros montant
+    for (const correspondingItem of this.dataCorresponding) {
+      const excelItem = this.dataFromExcel.find(excelItem => excelItem.numero === correspondingItem.numero);
+
+      if (excelItem && excelItem.montant !== correspondingItem.montant) {
+        this.dataMontantNoCor.push(correspondingItem);
+      }
+    }
+
+    // Comparaison des données inexistantes dans l'autre sens
+    for (const dbItem of this.dataFromDataBase) {
+      let numeroExisteDansExcel = false;
+
+      for (const excelItem of this.dataFromExcel) {
+        if (dbItem.numero === excelItem.numero) {
+          numeroExisteDansExcel = true;
+          break;
+        }
+      }
+
+      if (!numeroExisteDansExcel) {
+        this.dataNoExistExcel.push(dbItem);
+      }
+    }
+
+    // Mettre à jour les sources de données pour les tables
+    this.tableCorresponding = new MatTableDataSource(this.dataCorresponding);
+    this.tableMontantNoCor = new MatTableDataSource(this.dataMontantNoCor);
+    this.tableNoExistDB = new MatTableDataSource(this.dataNoExistDB);
+    this.tableNoExistExcel = new MatTableDataSource(this.dataNoExistExcel);
+  }
+
+
+
+
+
 
 }
